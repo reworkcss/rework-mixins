d925fbbb616645803e47a9ff5c53984767ea02ac
TJ Holowaychuk <tj@vision-media.ca>
2012-11-29 13:04:19 -0800

    add relative / fixed mixins

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 exports['border-radius'] = require('./lib/border-radius');
 exports['overflow'] = require('./lib/ellipsis');
 exports['absolute'] = require('./lib/absolute');
+exports['relative'] = require('./lib/relative');
+exports['fixed'] = require('./lib/fixed');
