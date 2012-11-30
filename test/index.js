755034817395ac764e7426f12cce0467b396daf8
TJ Holowaychuk <tj@vision-media.ca>
2012-11-30 15:56:35 -0800

    alter test runner

--- test/index.js
+++ test/index.js
@@ -6,19 +6,19 @@ var path = require('path');
 var read = fs.readFileSync;
 var readdir = fs.readdirSync;
 
-readdir('test/cases').forEach(function(file){
-  if (~file.indexOf('.out')) return;
-  var base = path.basename(file, '.css');
-  var input = read('test/cases/' + file, 'utf8');
-  var output = read('test/cases/' + base + '.out.css', 'utf8');
+describe('should support', function(){
+  readdir('test/cases').forEach(function(file){
+    if (~file.indexOf('.out')) return;
+    var base = path.basename(file, '.css');
+    var input = read('test/cases/' + file, 'utf8');
+    var output = read('test/cases/' + base + '.out.css', 'utf8');
   
-  describe(base, function(){
-    it('should work', function(){
+    it(base, function(){
       var css = rework(input)
         .use(rework.mixin(mixins))
         .toString();
-      
+
       css.should.equal(output);
     })
-  })
-});
+  });
+})
